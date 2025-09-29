50a6f43eb8dfbfc320cfdbbc89b76138cde53c1e
Henry <hbehr001@stpaul.k12.mn.us>
2025-09-28 22:50:12 -0500

    FileOpener complete

--- src/FileOpener.java
+++ src/FileOpener.java
@@ -1,21 +1,39 @@
-
 import java.io.*;
 
 public class FileOpener {
-    public static void main(String[] args)
-    {
-        try
-        {
-            BufferedReader in = new BufferedReader(new FileReader("stuff.txt"));
+    static int totalCount = 0;
+    static int charCount = 0;
+    static int[] charCountList = new int[26];
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/stuff.txt"));
             String str;
 
-            while ((str = in.readLine()) != null)
-            {
-                System.out.println(str);
-            }
+            while ((str = in.readLine()) != null) {
+                totalCount += str.length();
 
+                for (int i = 0; i < 26; i++) {
+                    charCountList[i] += countChars(str, i + 97);
+
+                }
+            }
+            System.out.println("Total characters: " + totalCount);
+            for (int i = 0; i < charCountList.length; i++ ) {
+                System.out.println((char)(i + 97) + ": " + Math.round((double)charCountList[i]/totalCount * 100) + "%");
+            }
+        } catch (IOException e) {
         }
-        catch (IOException e) {
+    }
+
+    public static int countChars(String str, int character) {
+        int countChar = 0;
+
+        for (int c = 0; c < str.length(); c++) {
+            if (str.charAt(c) == character) {
+                countChar++;
+            }
         }
+        return countChar;
     }
 }
